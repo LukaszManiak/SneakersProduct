0637908f17ab518df7b597589f9dff5a75153bea
Lukasz <lukasz.maniak22@gmail.com>
2023-06-13 21:03:15 +0200

    commit message

---
+++ README.md
@@ -0,0 +1,51 @@
+## Welcome! 👋
+
+# Frontend Mentor - E-commerce product page solution
+
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6).
+
+## Table of contents
+
+- [Overview](#overview)
+- [The challenge](#the-challenge)
+  - [Links](#links)
+  - [Built with](#built-with)
+- [Author](#author)
+- [Screenshots](#screenshot)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/Mejniak/Frontend-Mentor-Ecommerce-product-page-main)
+<!-- - Live Site URL: [Netlify](https://bookmarklanding-lukas.netlify.app/) -->
+
+### Built with
+
+- Semantic HTML5 markup
+- SCSS
+- Flexbox
+- JavaScript
+
+## Author
+
+- Frontend Mentor - [Lukasz Maniak](https://www.frontendmentor.io/profile/Mejniak)
+- GitHub- [Lukasz Maniak](https://github.com/Mejniak)
+
+### Screenshot
+
+![Screenshot 1](/screenshots/screen1.jpg?raw=true 'Screenshot 1')
+![Screenshot 2](/screenshots/screen2.jpg?raw=true 'Screenshot 2')
+![Screenshot 3](/screenshots/screen3.jpg?raw=true 'Screenshot 3')
+![Screenshot 4](/screenshots/screen4.jpg?raw=true 'Screenshot 4')
---
+++ app.js
@@ -0,0 +1,172 @@
+'use strict';
+
+//elements
+const navOpen = document.querySelector('.nav-open-btn');
+const navClose = document.querySelector('.nav-close-btn');
+const navWhiteBg = document.querySelector('.nav-white-bg');
+const navDarkBg = document.querySelector('.nav-dark-bg');
+
+const addProductCount = document.querySelector('.add-count-button');
+const removeProductCount = document.querySelector('.remove-count-button');
+
+const productCount = document.querySelector('.count-p');
+
+const cartContainer = document.querySelector('.cart-container');
+const productCountAlert = document.querySelector('.product-count');
+const addToCartBtn = document.querySelector('.add-to-cart-button');
+const deleteFromCartBtn = document.querySelector('.delete-button');
+const cartBtn = document.querySelector('.cart-button');
+
+const thumbnailsContainer = document.querySelector(
+  '.thumbnails-images-container'
+);
+
+const imagesContainer = document.querySelector('.product-images-container');
+const allThumbnails = document.querySelectorAll('.product-thumbnail');
+
+const productImage = document.querySelector('.product-image');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+// nav menu
+
+//setting hidden class
+navWhiteBg.classList.add('hidden');
+navDarkBg.classList.add('hidden');
+let isOpen = false;
+
+//functions for closing/opening nav menu
+const openNavMenu = function () {
+  navWhiteBg.classList.remove('hidden');
+  navDarkBg.classList.remove('hidden');
+
+  isOpen = !isOpen;
+};
+const closeNavMenu = function () {
+  navWhiteBg.classList.add('hidden');
+  navDarkBg.classList.add('hidden');
+
+  isOpen = !isOpen;
+};
+
+navOpen.addEventListener('click', openNavMenu);
+navClose.addEventListener('click', closeNavMenu);
+
+//setting product count
+let count = 0;
+
+addProductCount.addEventListener('click', function () {
+  count++;
+  productCount.innerText = count;
+});
+
+removeProductCount.addEventListener('click', function () {
+  if (count == 0) return;
+
+  count--;
+  productCount.innerText = count;
+});
+
+//adding product to cart
+
+//default parameters
+let cartPrice;
+let finalCount = 0;
+cartContainer.insertAdjacentHTML('afterbegin', emptyCart());
+
+//case1: returning html for empty cart
+
+function emptyCart() {
+  return `
+    <h4>Cart</h4>
+    <p class="empty-cart-p">Your cart is empty.</p>
+    `;
+}
+//case2:returning html for product in cart
+function productCart(count) {
+  finalCount += +count;
+  return `
+  
+<h4>Cart</h4>
+
+  <div class="cart-item">
+    <img class="" src="images/image-product-1.jpg" alt="" />
+    <div>
+      <p class="cart-item-product-title">Fall Limited Edition Sneakers</p>
+      <p class="cart-item-product-price">$125.00 x ${+finalCount}<span> $${
+    +finalCount * 125
+  }</span></p>
+    </div>
+  <button onClick=deleteProduct() class="delete-button">
+    <img src="images/icon-delete.svg" alt="" />
+  </button>
+  </div>
+  <button class="orange-button">Checkout</button>
+`;
+}
+
+addToCartBtn.addEventListener('click', function () {
+  cartContainer.innerHTML = '';
+  if (!+productCount.innerText) {
+    cartContainer.insertAdjacentHTML('afterbegin', emptyCart());
+  } else {
+    cartContainer.insertAdjacentHTML('afterbegin', productCart(count));
+    productCountAlert.innerText = finalCount;
+    count = 0;
+    productCount.innerText = count;
+    productCountAlert.classList.remove('hidden');
+  }
+});
+
+//delete product from cart
+const deleteProduct = function () {
+  cartContainer.innerHTML = '';
+  cartContainer.insertAdjacentHTML('afterbegin', emptyCart());
+  count = 0;
+  finalCount = 0;
+  productCountAlert.innerText = count;
+  cartContainer.classList.toggle('hidden');
+  productCountAlert.classList.add('hidden');
+};
+
+//show cart
+cartBtn.addEventListener('click', function () {
+  cartContainer.classList.toggle('hidden');
+});
+
+//showing selected image
+let currentImage = 1;
+
+//mobile view/modal window
+prevBtn.addEventListener('click', function () {
+  if (currentImage == 1) return;
+  currentImage--;
+  productImage.src = `images/image-product-${currentImage}.jpg`;
+  toggleFocus(currentImage);
+  console.log(currentImage);
+});
+
+nextBtn.addEventListener('click', function () {
+  if (currentImage == 4) return;
+  currentImage++;
+  productImage.src = `images/image-product-${currentImage}.jpg`;
+  console.log(currentImage);
+  toggleFocus(currentImage);
+});
+
+//full view
+allThumbnails.forEach(e => {
+  e.addEventListener('click', function () {
+    currentImage = +e.dataset.thumbnail;
+    productImage.src = `images/image-product-${currentImage}.jpg`;
+    toggleFocus(currentImage);
+  });
+});
+
+function toggleFocus(curNum) {
+  allThumbnails.forEach(e => {
+    curNum == +e.dataset.thumbnail
+      ? e.classList.add('image-selected')
+      : e.classList.remove('image-selected');
+  });
+}
